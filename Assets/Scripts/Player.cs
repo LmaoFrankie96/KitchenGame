f32dd21f5824bd4df96db5dd18b602b342c6f313
Muhammad Ramish <mramish9696@gmail.com>
2024-03-15 10:32:39 +0500

    - initial player movement done

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+    private Vector2 inputVector;
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+    private void PlayerMovement()
+    {
+
+        inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            inputVector.x = +1;
+        }
+        inputVector = inputVector.normalized;
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        Debug.Log(inputVector);
+    }
+}
