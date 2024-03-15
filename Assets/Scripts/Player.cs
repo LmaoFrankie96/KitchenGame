66a112741b064372915db65697ace65d4495a940
Muhammad Ramish <mramish9696@gmail.com>
2024-03-15 10:46:38 +0500

    - player visual and rotation added with movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotateSpeed = 10f;
     private Vector2 inputVector;
 
     private void Update()
@@ -38,6 +39,7 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
-        Debug.Log(inputVector);
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        //Debug.Log(inputVector);
     }
 }
