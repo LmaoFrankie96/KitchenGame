37521b1cf9b5e949b2f1d55f90ad3296027367bb
Muhammad Ramish <mramish9696@gmail.com>
2024-03-18 11:15:25 +0500

    - applied new input system

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,5 +2,46 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    
+    private Vector2 inputVector;
+    private static InputManager instance;
+    private PlayerInputActions inputActions;
+    public static InputManager Instance
+    {
+        get
+        {
+
+            if (instance == null)
+            {
+
+                instance = FindObjectOfType<InputManager>();
+                if (instance != null)
+                {
+
+                    DontDestroyOnLoad(instance.gameObject);
+                }
+
+            }
+            return instance;
+        }
+
+    }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        inputActions = new PlayerInputActions();
+        inputActions.Player.Enable();
+    }
+    public Vector2 GetMovementVector()
+    {
+
+        inputVector = inputActions.Player.Move.ReadValue<Vector2>();
+        inputVector = inputVector.normalized;
+
+        return inputVector;
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,36 +17,15 @@ public class Player : MonoBehaviour
     }
     private void PlayerMovement()
     {
-
-        inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-
-            inputVector.x = +1;
-        }
-        inputVector = inputVector.normalized;
+        inputVector = InputManager.Instance.GetMovementVector();
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         isWalking = moveDir != Vector3.zero;
         //Debug.Log(inputVector);
     }
-    public bool IsWalking() {
+    public bool IsWalking()
+    {
 
         return isWalking;
     }
