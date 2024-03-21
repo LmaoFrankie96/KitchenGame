3d5a61c5cf714fd2a44aca9d8fe1cf73fb9b68e7
Muhammad Ramish <mramish9696@gmail.com>
2024-03-21 12:18:00 +0500

    - added logic to let counters know if they have a kitchen object on them

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -12,15 +12,16 @@ public class ClearCounter : MonoBehaviour
 
     private void Update()
     {
-        if (testing && Input.GetKeyDown(KeyCode.T)) {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
 
             if (kitchenObject != null)
             {
 
                 kitchenObject.SetClearCounter(secondClearCounter);
-                Debug.Log(kitchenObject.GetClearCounter());
+                
             }
-          
+
         }
     }
     public void Interact()
@@ -29,21 +30,36 @@ public class ClearCounter : MonoBehaviour
         if (kitchenObject == null)
         {
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-            kitchenObjectTransform.localPosition = Vector3.zero;
-
-            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
-            kitchenObject.SetClearCounter(this);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
 
         }
         else
         {
-           Debug.Log(kitchenObject.GetClearCounter());
+            Debug.Log(kitchenObject.GetClearCounter());
         }
     }
 
-    public Transform GetKitchenObjectFollowTransform() {
+    public Transform GetKitchenObjectFollowTransform()
+    {
 
 
         return counterTopPoint;
+    }
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+
+        this.kitchenObject = kitchenObject;
+    }
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+    public bool HasKitchenObject()
+    {
+        return this.kitchenObject != null;
     }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -5,12 +5,23 @@ public class KitchenObject : MonoBehaviour
     [SerializeField] private KitchenObjectsSO kitchenObjectsSO;
     private ClearCounter clearCounter;
     public KitchenObjectsSO GetKitchenObjectsSO() { return kitchenObjectsSO; }
-    public void SetClearCounter(ClearCounter mClearCounter) { 
-    
+    public void SetClearCounter(ClearCounter mClearCounter)
+    {
+
+        if (this.clearCounter != null)
+        {
+            this.clearCounter.ClearKitchenObject();
+        }
         this.clearCounter = mClearCounter;
-        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+
+        if (clearCounter.HasKitchenObject()) {
+
+            Debug.LogError("Counter already has a kitchen object!!!");
+        }
+        mClearCounter.SetKitchenObject(this);
+        transform.parent = mClearCounter.GetKitchenObjectFollowTransform();
         transform.localPosition = Vector3.zero;
     }
-    public ClearCounter GetClearCounter() { return this.clearCounter; } 
+    public ClearCounter GetClearCounter() { return this.clearCounter; }
 
 }
