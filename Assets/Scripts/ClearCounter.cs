e3f5e4a96555ea3f891d1febf6efcbd35f656f7e
Muhammad Ramish <mramish9696@gmail.com>
2024-03-22 12:15:36 +0500

    - player is able to drop and collect a single item from a clear counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -6,7 +6,32 @@ public class ClearCounter : BaseCounter, IKitchenObjectParent
 
     public override void Interact(Player player)
     {
+        if (!HasKitchenObject())
+        {
 
+            //has no kitchen object on it
+            if (player.HasKitchenObject())
+            {
+                //Player carrying kitchen object
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else { 
+            //Player carrying nothing
+            }
+        }
+        else {
+            //has kitchen object on it
+            if (player.HasKitchenObject())
+            {
+                //Player carrying kitchen object
+                
+            }
+            else
+            {
+                //Player carrying nothing
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
        
     }
 
